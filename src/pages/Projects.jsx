8bfcea9974766d8d603cba17188eb7494bffc5a1
Achieve-software <vtcbcdtyr@gmail.com>
2023-12-22 23:59:56 +0300

    ff

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import {Link} from "react-router-dom";
 import PROJECTS from "./../data";
+import Form from "react-bootstrap/Form";
 
 const Projects = () => {
-   
+  const [search, setSearch] = useState("");
   return (
     <main> 
       <div className="pg-header">
       <h1>My Projects</h1>
       </div>
 
-
+ 
+      <Form.Control
+        placeholder="Search a project..."
+        type="search"
+        className="w-25 m-auto mt-5 "
+        style={{ border: '1px solid #080F36' }}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
       <div className="container content">
         <div className="row products-row">
-          {PROJECTS.map((projects) => {
+          
+       {PROJECTS.filter((projects) =>
+              projects.name.toLowerCase().includes(search.trim().toLowerCase())
+            ).map((projects) => { 
+              
             return (
+              
               <div className="col-lg-4" key={projects.id}>
                 <div className="card">
                 <div className="img-wrap"  type="button"
