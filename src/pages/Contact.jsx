12338243d52b607084c324483ec6e5df083a3e68
Achieve-software <vtcbcdtyr@gmail.com>
2023-12-23 14:24:20 +0300

    cont

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Soci from '../components/Soci'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="w-full   flex flex-col items-center justify-center mt-10 ">
+        <Soci />
+    </div>
   )
 }
 
