4b55d417b8f27f4fc14919655df64f8f669e84c4
Achieve-software <vtcbcdtyr@gmail.com>
2024-01-26 02:50:30 +0300

    d

--- src/components/Type.jsx
+++ src/components/Type.jsx
@@ -10,7 +10,6 @@ function Type() {
           // "<span style='font-family: Courier;font-size: 20px'>Html-Css-Sass-Bootstrap-React-Material UI-React Bootstarp-Axios-Tailwind-Styled Components-JavaScript-TypeScript-JQuery-Node.js-Express.js-Django-SQL-Sqlite-PostgreSql-MySql-MongoDb</span> ",
 
           "<span style='font-family: Courier;font-size: 28px'>React-Material UI-Axios-Tailwind-Styled Components-React Bootstarp</span> ",
-          
           "<span style='font-family: Courier;font-size: 28px'>Html-Css-Sass-Bootstrap</span> ",
           "<span style='font-family: Courier;font-size: 28px'>JavaScript-TypeScript-JQuery</span> ",
           "<span style='font-family: Courier;font-size: 28px'>Node.js-Express.js-Django</span> ",
