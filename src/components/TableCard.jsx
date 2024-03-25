fc94aef871f5149ffca3fd643a7f7171b499963b
Achieve-software <vtcbcdtyr@gmail.com>
2024-03-25 23:41:35 +0300

    ff

--- src/components/TableCard.jsx
+++ src/components/TableCard.jsx
@@ -5,6 +5,7 @@ const TableCard = () => {
    
 <div className="group origin-bottom-right duration-500 -rotate-12 hover:-rotate-0 hover:-skew-x-12 skew-x-0 hover:-translate-x-6  hover:translate-y-12">
   <div className="duration-500 group-hover:duration-400 relative rounded-2xl w-40 h-24 bg-zinc-800 text-gray-50 flex flex-col justify-center items-center gap-1 before:-skew-x-12  before:rounded-2xl  before:absolute before:content['']  before:bg-neutral-700 before:right-3 before:top-0 before:w-40 before:h-20 before:-z-10 group-hover:before:-right-3 group-hover:before:skew-x-12 before:duration-500 group-hover:duration-500">
+    <span className="text-2xl font-bold">Akif Yılmaz</span>
     <p className="text-amber-300 font-thin">-Full Stack Developer-<br />Front-End & Back-End</p>
   </div>
 </div>
